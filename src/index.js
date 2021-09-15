e6b994a60e4e100c71c17089735be3fbab40a104
François Michaud <fmichaud@deepnox.io>
2021-09-15 14:54:10 +0200

    Notes de cours

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 const express = require('express')
+const crypto = require('crypto')
+
+
+
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
+})
+
+app.get('/sha1', (req, res) => {
+    const s = req.query.s
+    const shasum = crypto.createHash('sha1')
+    shasum.update(s)
+    const hashedValue = shasum.digest('hex') // => "0beec7b5ea3f0fdbc95d0dd47f3c5bc275da8a33"
+
+    res.json({"hash": hashedValue})
 })
 
 app.listen(port, () => {
