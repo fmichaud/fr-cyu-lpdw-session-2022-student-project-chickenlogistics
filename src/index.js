f4080848811d0de32713fda6dfc7468dc309249e
François Michaud <fmichaud@deepnox.io>
2021-09-17 11:54:48 +0200

    Body parser missing: fixed

--- src/index.js
+++ src/index.js
@@ -2,6 +2,14 @@ const express = require('express')
 const crypto = require('crypto')
 const db = require('../config/databases')
 
+const bodyParser = require('body-parser');
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+app.use(router)
+
 
 
 // Authenticate.
--- src/routers/cities.api.js
+++ src/routers/cities.api.js
@@ -1,12 +1,16 @@
-
 const express = require('express');
 const router = express.Router();
 
 const db = require('../../config/databases');
-const { City } = require('../models');
+const {City} = require('../models');
 
 router.get('/', (req, res, next) => {
+  City.findAll()
+    .then(cities => {
+      console.log(cities)
+      res.sendStatus(200);
+    })
+    .catch(err => console.log(err))
 
 })
-
 
