ea56dcc899b86b1dcc9e40750caa7c8aa6f7dd35
François Michaud <fmichaud@deepnox.io>
2021-09-17 12:19:45 +0200

    Missing exports fixed & enhance API urls of routers

--- src/routers/cities.api.js
+++ src/routers/cities.api.js
@@ -8,9 +8,23 @@ router.get('/', (req, res, next) => {
   City.findAll()
     .then(cities => {
       console.log(cities)
-      res.sendStatus(200);
+      res.status(200).json(cities);
     })
-    .catch(err => console.log(err))
+    .catch(err => console.error(err))
 })
+
+/**
+ A ne pas utiliser tout de suite ! pour demi-journée 6.
+
+router.get('/async', async (req, res, next) => {
+  try {
+    let cities = await City.findAll()
+    res.sendStatus(200)
+  } catch (e) {
+    console.error(`Error:`)
+    throw e
+  }
+})
+ */
 
 module.exports = router
